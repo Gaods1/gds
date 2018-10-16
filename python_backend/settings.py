6360d99754921aaea01defa749dfa1216b89a159
14020 <1402094269@qq.com>
2018-10-16 14:48:19 +0800

    配置mysql数据库

--- python_backend/settings.py
+++ python_backend/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'python_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'patclub',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD':'123456'
     }
 }
 
