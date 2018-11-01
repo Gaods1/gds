d0f4f23ff63346fbf67ca9ac50b82847d7072bcf
tonbochow <tonbochow@qq.com>
2018-11-01 16:30:40 +0800

    role2

--- account/serializers.py
+++ account/serializers.py
@@ -1,4 +1,5 @@
 from account.models import AccountInfo
+from account.models import RoleInfo
 from rest_framework import serializers
 
 
@@ -8,4 +9,13 @@ class AccountInfoSerializer(serializers.HyperlinkedModelSerializer):
         # fields = ('url','serial','account_code', 'account', 'password', 'state', 'dept_code','account_memo',
         #           'user_name', 'account_id', 'user_mobile', 'user_email', 'creater', 'insert_time',
         #           'update_time', 'last_login')
+        fields = '__all__'
+
+
+"""
+RoleInfo serializer
+"""
+class RoleInfoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = RoleInfo
         fields = '__all__'
--- account/views.py
+++ account/views.py
@@ -4,6 +4,9 @@ from account.models import AccountInfo
 from account.serializers import AccountInfoSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from account.models import RoleInfo
+from account.serializers import RoleInfoSerializer
+from misc.misc import gen_uuid32
 # Create your views here.
 
 
@@ -19,3 +22,21 @@ class AccountViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+"""
+角色管理
+"""
+class RoleInfoViewSet(viewsets.ModelViewSet):
+    queryset = RoleInfo.objects.all().order_by('serial')
+    serializer_class = RoleInfoSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data['creater'] = request.user.account
+        data['role_code'] = gen_uuid32()
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data,status=status.HTTP_201_CREATED,headers=headers)
