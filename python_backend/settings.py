d4a47ae1d586cfa7a5a12e8253e4ec03013001c1
14020 <1402094269@qq.com>
2018-11-12 10:05:17 +0800

    增加swagger文档接口

--- python_backend/settings.py
+++ python_backend/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_jwt',
     'django_filters',
+    'rest_framework_swagger',
     'corsheaders',# 跨域(以后删除)
 ]
 
--- python_backend/urls.py
+++ python_backend/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework import routers
 from account import views
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
 
 
 router = routers.DefaultRouter()
@@ -31,8 +33,13 @@ router.register(r'dept',views.DeptinfoViewSet)
 router.register(r'param',views.ParamInfoViewSet)
 router.register(r'district',views.SystemDistrictViewSet)
 
+
+schema_view = get_schema_view(title='Users API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-token-auth/', obtain_jwt_token, name='auth-jwt-get'),
+    path('docs/', schema_view, name="docs"),
     path('api/', include(router.urls))
 ]
