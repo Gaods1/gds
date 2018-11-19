c8f7f2bdab8c7e1a410fa10c9b8ca6394dedf020
tonbochow <tonbochow@qq.com>
2018-11-19 16:16:48 +0800

    征询管理初始化版本

--- consult/urls.py
+++ consult/urls.py
@@ -1,7 +1,12 @@
 from rest_framework import routers
+from .views import *
 from django.urls import path, include, re_path
 
 router = routers.DefaultRouter()
+router.register(r'consult_info', ConsultInfoViewSet)
+router.register(r'consult_reply_info', ConsultReplyInfoViewSet)
+router.register(r'consult_checkinfo', ConsultCheckinfoViewSet)
+router.register(r'consult_reply_checkinfo', ConsultReplyCheckinfoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- consult/views.py
+++ consult/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from consult.models import *
+from consult.serializers import *
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import filters
+import django_filters
 
 
 # Create your views here.
+
+#征询管理
+class ConsultInfoViewSet(viewsets.ModelViewSet):
+    queryset = ConsultInfo.objects.all().order_by('-serial')
+    serializer_class = ConsultInfoSerializer
+
+
+#专家征询回复管理
+class ConsultReplyInfoViewSet(viewsets.ModelViewSet):
+    queryset = ConsultReplyInfo.objects.all().order_by('-serial')
+    serializer_class = ConsultReplyInfoSerializer
+
+
+#征询审核管理
+class ConsultCheckinfoViewSet(viewsets.ModelViewSet):
+    queryset = ConsultCheckinfo.objects.all().order_by('-serial')
+    serializer_class = ConsultCheckinfoSerializer
+
+#专家征询回复审核管理
+class ConsultReplyCheckinfoViewSet(viewsets.ModelViewSet):
+    queryset = ConsultReplyCheckinfo.objects.all().order_by('-serial')
+    serializer_class = ConsultReplyCheckinfoSerializer
