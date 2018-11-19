fcf1b509b540067ccbcaace807f7a1021eee9629
tonbochow <tonbochow@qq.com>
2018-11-19 16:49:17 +0800

    征询管理初始化版本

---
+++ consult/serializers.py
@@ -0,0 +1,63 @@
+from consult.models import *
+from rest_framework import serializers
+
+#征询表序列化
+class ConsultInfoSerializer(serializers.ModelSerializer):
+    consult_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    consult_endtime = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    insert_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    class Meta:
+        model = ConsultInfo
+        fields = ['serial',
+                  'consult_code',
+                  'consulter',
+                  'consult_memo',
+                  'consult_body',
+                  'consult_time',
+                  'consult_endtime',
+                  'consult_state',
+                  'insert_time',
+                  'creater',]
+
+#专家征询回复表序列化
+class ConsultReplyInfoSerializer(serializers.ModelSerializer):
+    reply_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    class Meta:
+        model = ConsultReplyInfo
+        fields = ['serial',
+                  'reply_code',
+                  'expert_code',
+                  'consult_code',
+                  'reply_body',
+                  'reply_time',
+                  'reply_state',]
+
+
+
+#征询审核表序列化
+class ConsultCheckinfoSerializer(serializers.ModelSerializer):
+    check_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    class Meta:
+        model = ConsultCheckinfo
+        fields = ['serial',
+                  'consult_code',
+                  'consult_pmemo',
+                  'consult_pmody',
+                  'check_time',
+                  'check_state',
+                  'check_memo',
+                  'checker',]
+
+
+
+#专家征询回复审核表序列化
+class ConsultReplyCheckinfoSerializer(serializers.ModelSerializer):
+    check_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
+    class Meta:
+        model = ConsultReplyCheckinfo
+        fields = ['serial',
+                  'reply_code',
+                  'check_time',
+                  'check_state',
+                  'check_memo',
+                  'checker',]
