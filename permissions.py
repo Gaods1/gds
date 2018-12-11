2d9c5dc441dc45cd729705e6359e4909ea37bc24
14020 <1402094269@qq.com>
2018-12-11 10:43:58 +0800

    权限修改

--- permissions.py
+++ permissions.py
@@ -1,35 +1,37 @@
 from rest_framework import permissions
 from account.models import AccountInfo, FunctionInfo, AccountRoleInfo, RoleInfo, RoleFuncInfo, AccountDisableFuncinfo
+import re
 
 
 class FuncPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        account = request.user
-        account_roles = AccountRoleInfo.objects.filter(account=account, state=1)
-        url = request.path
+        # 查询是否有这个功能点
+        url = re.search('^(/.*?/.*?/)(\d*)', request.path).group(1)
         try:
-            func = FunctionInfo.objects.get(func_url=url, state=1)
+            func_code = FunctionInfo.objects.values_list('func_code', flat=True).get(func_url=url, state=1)
         except Exception as e:
             return False
 
-        roles_code = []
-        for ar in account_roles:
-            rcode = RoleInfo.objects.get(role_code=ar.role_code, state=1)
-            if rcode:
-                roles_code.append(ar.role_code)
+        # 查询此用户是否禁掉了此功能点
+        account = request.user
+        disable_fuc = AccountDisableFuncinfo.objects.values_list('func_code', flat=True).filter(account=account, state=1)
+        if func_code in disable_fuc:
+            return False
 
-        for role_code in roles_code:
-            try:
-                role_func = RoleFuncInfo.objects.get(role_code=role_code, func_code=func.func_code, state=1)
-                try:
-                    account_disable_func = AccountDisableFuncinfo.objects.get(account=account, func_code=func.func_code,state=1)
-                except Exception as e:
-                    return True
-            except Exception as e:
-                pass
-
+        # 查询用户是否有当前功能点
+        account_roles = AccountRoleInfo.objects.values_list('role_code', flat=True).filter(account=account, state=1)
+        roles_code = RoleInfo.objects.values_list('role_code', flat=True).filter(role_code__in=account_roles, state=1)
+        funcs = RoleFuncInfo.objects.values_list('func_code', flat=True).filter(role_code__in=roles_code, state=1)
+        if func_code in funcs:
+            return True
         return False
+
+
+class DontCheckSelf(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        pass
 
 
 class ReadOnlyPermission(permissions.BasePermission):
