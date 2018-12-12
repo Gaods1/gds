2f6790e1436f4bd17e7491b7cc5df8dae24a7e04
xwp <xwp_fullstack@163.com>
2018-12-12 14:49:18 +0800

    成果需求初步测试完成 1212 下午(公共模块）

--- achievement/views.py
+++ achievement/views.py
@@ -986,7 +986,6 @@ class ManagementpViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data,status=status.HTTP_201_CREATED,headers=headers)
 
