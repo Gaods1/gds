972ffe67773c577150ad772bcaf8d2af7f0a111d
xwp <xwp_fullstack@163.com>
2018-12-12 13:37:01 +0800

    成果需求初步测试完成 1212 下午(公共模块）

--- public_models/utils.py
+++ public_models/utils.py
@@ -1,6 +1,7 @@
 import os
 
 import shutil
+import copy
 
 from backends import FileStorage
 from public_models.models import ParamInfo, AttachmentFileType, AttachmentFileinfo
@@ -93,12 +94,8 @@ def move_attachment(tname_attachment,ecode):
     absolute_path_front = ParamInfo.objects.get(param_code=3).param_value
     relative_path = ParamInfo.objects.get(param_code=2).param_value
     relative_path_front = ParamInfo.objects.get(param_code=4).param_value
-    #canshu = canshu
     tcode_attachment = AttachmentFileType.objects.get(tname=tname_attachment).tcode
-    #tcode_dange = AttachmentFileType.objects.get(tname='publishResultCover').tcode
-    ecode = ecode
     files_fujian = AttachmentFileinfo.objects.filter(tcode=tcode_attachment, ecode=ecode, state=1)
-    #files_dange = AttachmentFileinfo.objects.get(tcode=tcode_dange, ecode=ecode, state=1)
     dict = {'xiaofan':123}
     # 遍历所有状态下的对象(附件)
     for file in files_fujian:
@@ -114,30 +111,41 @@ def move_attachment(tname_attachment,ecode):
 
             # 将状态改为审核通过
             file.operation_state = 3
+            file.save()
             # 将临时文件转为正式文件
-            url_j = '{}{}{}'.format(absolute_path, file.path,file.file_name)
-            if os.path.exists(url_j):
+            url_j_c = '{}{}{}'.format(absolute_path, file.path,file.file_name)
+            if os.path.exists(url_j_c):
+
+                #更新file.path中的ecode为统一的ecode
+                file_path = file.path[:-1]
+                file_list = file_path.split('/')
+                path_ecode = file_list.pop()
+                file_list.append(ecode)
+                file.path = '/'.join(file_list) + '/'
+                file.save()
+
+                #更新绝对路径并转移文件
+                url_j = url_j_c.replace(path_ecode,ecode)
                 url_x = url_j.replace(absolute_path, relative_path)
-                shutil.move(url_j, url_x)
+                shutil.move(url_j_c, url_x)
+
+                #删除临时目录
+                url_j_c_list = url_j_c.split('/')
+                del url_j_c_list[-1]
+                url_j_c_s = '/'.join(url_j_c_list)
+                os.rmdir(url_j_c_s)
 
                 # 给前端抛路径以及状态
                 url_x_q = url_x.replace(relative_path, relative_path_front)
                 dict[url_x_q] = file.operation_state
     return dict
-    # serializer.data = {**serializer.data,**dict}
-    # serializer.data['fujian'] = dict
-
 
 def move_single(tname_singgle,ecode):
     absolute_path = ParamInfo.objects.get(param_code=1).param_value
     absolute_path_front = ParamInfo.objects.get(param_code=3).param_value
     relative_path = ParamInfo.objects.get(param_code=2).param_value
     relative_path_front = ParamInfo.objects.get(param_code=4).param_value
-    #canshu = canshu
-    #tcode_fujian = AttachmentFileType.objects.get(tname='publishResultAttach').tcode
     tcode_single = AttachmentFileType.objects.get(tname=tname_singgle).tcode
-    ecode = ecode
-    #files_fujian = AttachmentFileinfo.objects.filter(tcode=tcode_fujian, ecode=ecode, state=1)
     files_dange = AttachmentFileinfo.objects.filter(tcode=tcode_single, ecode=ecode, state=1)
     dict = {'xiaofan':123}
     # 遍历所有状态下的对象(单个文件)
@@ -151,14 +159,30 @@ def move_single(tname_singgle,ecode):
             if os.path.exists(url):
                 os.remove(url)
         else:
-
             # 将状态改为审核通过
             file.operation_state = 3
+            file.save()
             # 将临时文件转为正式文件
-            url_j = '{}{}{}'.format(absolute_path, file.path,file.file_name)
-            if os.path.exists(url_j):
+            url_j_c = '{}{}{}'.format(absolute_path, file.path, file.file_name)
+            if os.path.exists(url_j_c):
+                # 更新file.path中的ecode为统一的ecode
+                file_path = file.path[:-1]
+                file_list = file_path.split('/')
+                path_ecode = file_list.pop()
+                file_list.append(ecode)
+                file.path = '/'.join(file_list) + '/'
+                file.save()
+
+                # 更新绝对路径并转移文件
+                url_j = url_j_c.replace(path_ecode, ecode)
                 url_x = url_j.replace(absolute_path, relative_path)
-                shutil.move(url_j, url_x)
+                shutil.move(url_j_c, url_x)
+
+                # 删除临时目录
+                url_j_c_list = url_j_c.split('/')
+                del url_j_c_list[-1]
+                url_j_c_s = '/'.join(url_j_c_list)
+                os.rmdir(url_j_c_s)
 
                 # 给前端抛路径以及状态
                 url_x_q = url_x.replace(relative_path, relative_path_front)
@@ -187,3 +211,33 @@ def move_single(tname_singgle,ecode):
     # 上传服务器
     #list_url = obj._save(list_url, files)
     #return list_url
+
+
+
+
+#*************************************************************************************
+    # 遍历所有状态下的对象(单个文件)
+    #for file in files_dange:
+        # 找出伪删除的对象并从表中删除
+        #if file.operation_state == 2:
+            #file.delete()
+            #url = '{}{}{}'.format(relative_path, file.path, file.file_name)
+
+            # 找出该路径下是否有文件并删除
+            #if os.path.exists(url):
+                #os.remove(url)
+        #else:
+
+            # 将状态改为审核通过
+            #file.operation_state = 3
+            #file.save()
+            # 将临时文件转为正式文件
+            #url_j = '{}{}{}'.format(absolute_path, file.path, file.file_name)
+            #if os.path.exists(url_j):
+                #url_x = url_j.replace(absolute_path, relative_path)
+                #shutil.move(url_j, url_x)
+
+                # 给前端抛路径以及状态
+                #url_x_q = url_x.replace(relative_path, relative_path_front)
+                #dict[url_x_q] = file.operation_state
+    #return dict
