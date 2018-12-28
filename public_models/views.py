ad43b3ac9f6c24128fe7a09a28d606321d99ed43
tonbochow <tonbochow@qq.com>
2018-12-28 11:34:16 +0800

    领域专业管理功能前后台完成

--- public_models/views.py
+++ public_models/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from account.models import Deptinfo
+from public_models.models import MajorInfo
+from public_models.serializers import *
+from rest_framework import filters
+import django_filters
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -20,3 +25,27 @@ class BaseModelViewSet(viewsets.ModelViewSet):
         self.get_dept_codes(request.user.dept_code)
         return request
 
+
+#领域类型基本信息表（领域专家、经纪人、项目团队、成果、需求共用）
+
+class MajorInfoViewSet(viewsets.ModelViewSet):
+    queryset = MajorInfo.objects.all().order_by('state', '-serial')
+    serializer_class = MajorInfoSerializers
+
+    filter_backends = (
+        filters.SearchFilter,
+        django_filters.rest_framework.DjangoFilterBackend,
+        filters.OrderingFilter,
+    )
+
+    ordering_fields = ("insert_time", "state", "mlevel")
+    filter_fields = ("state", "mlevel", "mcode", "pmcode", "mtype")
+    search_fields = ("mname", "mabbr")
+
+    def create(self, request, *args, **kwargs):
+        request.data['creater'] = request.user.account
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
