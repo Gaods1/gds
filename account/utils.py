82a2ca1aa50f568fc0df5b6c91b2276930398d9a
14020 <1402094269@qq.com>
2018-12-28 15:28:10 +0800

    数据有效性验证

--- account/utils.py
+++ account/utils.py
@@ -43,5 +43,23 @@ def validate_tel(value):
 
 # 验证身份证
 def validate_id(value):
-    if not re.match(r'[^\d{15}$),(^\d{18}$),(^\d{17}(\d|X|x)$)]', value):
+    if not re.match(r'(^\d{15}$)|(^\d{18}}$)|(^\d{17}(\d|X|x)$)', value):
         raise ValidationError('身份证号码格式错误')
+
+
+# 验证统一社会信用代码
+def validate_license(value):
+    if not re.match(r'[^_IOZSVa-z\W]{2}\d{6}[^_IOZSVa-z\W]{10}', value):
+        raise ValidationError('统一社会信用代码格式错误')
+
+
+# 验证邮政编码
+def validate_zipcode(value):
+    if not re.match(r'^[1-9]\d{5}$', value):
+        raise ValidationError('邮政编码格式错误')
+
+
+# 验证密码
+def validate_password(value):
+    if not re.match(r'^[\w\^\$\[\]\{\}`~!@#%&*()-=+?|;:,.<>]{6,16}$', value):
+        raise ValidationError('密码格式错误,大小写字母，数字, 符号组合的6-16位字符串')
