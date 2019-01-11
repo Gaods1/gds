0e0516e22ec2562dbdd80af27f9ead0cd712d372
xwp <xwp_fullstack@163.com>
2019-01-11 14:36:36 +0800

    成果需求

--- python_backend/settings.py
+++ python_backend/settings.py
@@ -184,8 +184,8 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        #"LOCATION": "redis://120.77.58.203:6379/1",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://120.77.58.203:6379/1",
+        #"LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
