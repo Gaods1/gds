fe8ae21fd18298e2990d5c4f8fb2f0b276779ad9
tonbochow <tonbochow@qq.com>
2019-01-17 15:21:26 +0800

    征询审核get方法造成报错修改

--- consult/views.py
+++ consult/views.py
@@ -152,9 +152,9 @@ class ConsultInfoViewSet(viewsets.ModelViewSet):
                             user_code = list(set(user_code_list))
 
                             #领域专家不能回复自己创建的征询
-                            consult_expert_info = ExpertBaseinfo.objects.get(account_code=consult_info.consulter)
-                            if consult_expert_info.expert_code in user_code:
-                                user_code.remove(consult_expert_info.expert_code)
+                            consult_expert_info = ExpertBaseinfo.objects.filter(account_code=consult_info.consulter)
+                            if consult_expert_info:
+                                user_code.remove(consult_expert_info[0].expert_code)
 
                             if len(user_code) >= 10:
                                 user_codes = random.sample(user_code, 10)
