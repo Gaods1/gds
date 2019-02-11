1ff6104a293c38ce9a9e30ca6f20fd64e832b5bc
tonbochow <tonbochow@qq.com>
2019-02-11 15:19:56 +0800

    专业领域添加编辑加入mcode+mname唯一条件判断

--- public_models/views.py
+++ public_models/views.py
@@ -44,8 +44,35 @@ class MajorInfoViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         request.data['creater'] = request.user.account
+        mname = request.data.get('mname')
+        mtype = request.data.get('mtype')
+        #判断同类型不能有同名
+        majorinfo_exists = MajorInfo.objects.filter(mname=mname,mtype=mtype).exists()
+        if majorinfo_exists:
+            return Response({"detail": { "detail": ["同类型同名称已存在"]}}, status=400)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        mname = request.data.get('mname')
+        mtype = request.data.get('mtype')
+        # 判断同类型不能有同名
+        majorinfo = MajorInfo.objects.filter(mname=mname, mtype=mtype).exclude(serial=instance.serial)
+        if majorinfo:
+            return Response({"detail": { "detail": ["同类型同名称已存在"]}}, status=400)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
