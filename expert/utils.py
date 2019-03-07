a1aec6ba4f56abcd712e2f23da9932d8cf63f62e
14020 <1402094269@qq.com>
2019-03-07 15:53:10 +0800

    修改ValidationError的导入信息。增加ValueError报错的证件号码验证

--- expert/utils.py
+++ expert/utils.py
@@ -3,6 +3,9 @@ import time,requests
 from .models import *
 import datetime
 from django.db.models import Q
+import os
+import shutil
+from enterpriseperson.serializers import PersonalInfoSerializers
 
 
 # 更新或创建个人信息
@@ -121,6 +124,99 @@ def get_major(user_type, user_code):
 
 
 # 验证前端账号是否具有当前信息
-def check_identity(account_code, identity):
-    if IdentityAuthorizationInfo.objects.filter(account_code=account_code, identity_code=identity):
+def check_identity(account_code, identity, info):
+    try:
+        IdentityAuthorizationInfo.objects.get(account_code=account_code, identity_code=identity)
         raise ValueError('所选账号已经认证当前身份')
+    except IdentityAuthorizationInfo.DoesNotExist:
+        identity = IdentityAuthorizationInfo.objects.create(**info)
+
+
+# 验证前端账号是否具有当前信息
+def check_identity2(account_code, identity, info):
+    identity = IdentityAuthorizationInfo.objects.filter(account_code=account_code, identity_code=identity)
+    if identity:
+        identity.update(**info)
+    else:
+        IdentityAuthorizationInfo.objects.create(**info)
+
+
+# 验证证件号码是否已注册
+def check_id(account_code, id_type, id):
+    p = PersonalInfo.objects.filter(pid_type=id_type, pid=id)
+    if p and p[0].account_code != account_code:
+        raise ValueError('此证件号码已被他人使用')
+
+
+# 根据account_code 来更新或者创建person_info
+def create_or_update_person(account_code, info):
+    check_id(account_code, info['pid_type'], info['pid'])
+    person = PersonalInfo.objects.filter(account_code=account_code)
+    if person:
+        person.update(**info)
+        return person[0].pcode
+    else:
+        p = PersonalInfo.objects.create(**info)
+        return p.pcode
+
+
+# 插入领域与身份相关表
+def crete_major(mtype, user_type, user_code, majors):
+    if majors:
+        MajorUserinfo.objects.filter(user_code=user_code).delete()
+        major_user_info = {
+            'mtype': mtype,
+            'user_type': user_type,
+            'user_code': user_code
+        }
+        major_user_info_list = []
+        for m in majors:
+            major_user_info['mcode'] = m
+            major_user_info_list.append(MajorUserinfo(**major_user_info))
+        MajorUserinfo.objects.bulk_create(major_user_info_list)
+
+
+# 图片url转路径
+def url_to_path(url):
+    # 默认替换临时， 当type为True为替换正式
+    path = None
+    if url:
+        formal_path = ParamInfo.objects.get(param_name='upload_dir').param_value  # 多媒体文件正式路径
+        formal_url = ParamInfo.objects.get(param_name='attachment_dir').param_value    # 读取多媒体文件的正式路径
+        temp_path = ParamInfo.objects.get(param_name='upload_temp_dir').param_value  # 多媒体文件临时路径
+        temp_url = ParamInfo.objects.get(param_name='attachment_temp_dir').param_value  # 读取多媒体文件的正式路径
+        if temp_url in url:
+            path = url.replace(temp_url, temp_path)
+        elif formal_url in url:
+            path = url.replace(formal_url, formal_path)
+    return path
+
+
+# 复制图片到正式路径
+def copy_img(url, identity, type, ecode, creater):
+    try:
+        upload_temp_dir = ParamInfo.objects.get(param_name='upload_temp_dir').param_value
+        if upload_temp_dir in url and os.path.isfile(url):
+            file_name = url.split('/')[-1]
+            formal_path = ParamInfo.objects.get(param_name='upload_dir').param_value
+            tcode = AttachmentFileType.objects.get(tname=type).tcode
+            file_formal_path = os.path.join(formal_path, identity, tcode, ecode)
+            file_path = os.path.join(file_formal_path, file_name)
+            if not os.path.exists(file_formal_path):
+                os.makedirs(file_formal_path)
+            formal_file = shutil.copyfile(url, file_path)
+            path = os.path.join(identity, tcode, ecode) + '/'
+            AttachmentFileinfo.objects.filter(ecode=ecode, tcode=tcode, file_name=file_name).delete()
+            attachment = AttachmentFileinfo.objects.create(ecode=ecode, tcode=tcode, file_format=1, file_name=file_name,
+                                                           state=1,publish=1, file_order=0, operation_state=3,
+                                                           creater=creater,path=path, file_caption=file_name)
+            return formal_file
+        return None
+    except Exception as e:
+        raise ValueError(e)
+
+
+# 删除附件
+def remove_img(url):
+    if url and os.path.isfile(url):
+        os.remove(url)
