cef07f9ac66093dc0f9d19d3f6b2afe954e0bc8c
14020 <1402094269@qq.com>
2019-03-14 23:16:06 +0800

    修改附件修改

--- expert/utils.py
+++ expert/utils.py
@@ -215,7 +215,6 @@ def copy_img(url, identity, img_type, ecode, creater):
     try:
         upload_temp_dir = ParamInfo.objects.get(param_name='upload_temp_dir').param_value
         if url and upload_temp_dir in url and os.path.isfile(url):
-            raise ValueError('ceshi')
             file_name = url.split('/')[-1]
             formal_path = ParamInfo.objects.get(param_name='upload_dir').param_value
             tcode = AttachmentFileType.objects.get(tname=img_type).tcode
@@ -230,7 +229,6 @@ def copy_img(url, identity, img_type, ecode, creater):
                                               state=1, publish=1, file_order=0, operation_state=3,
                                               creater=creater, path=path, file_caption=file_name)
             return formal_file
-        raise ValueError('123')
         return None
     except Exception as e:
         raise ValueError(e)
