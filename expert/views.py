0bc023d0530a451c4546f86555f92d1c014cb464
14020 <1402094269@qq.com>
2019-03-15 17:04:30 +0800

    测试

--- expert/views.py
+++ expert/views.py
@@ -3515,6 +3515,7 @@ class TeamBaseinfoViewSet(viewsets.ModelViewSet):
 
                     # 插入领域相关
                     crete_major(2, 2, ecode, major)
+                    raise ValueError('测试断电')
 
                     # 复制图片到正式目录
                     formal_idfront = copy_img(idfront, 'Prteam', 'identityFront', ecode, creater)
