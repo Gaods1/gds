fec6451b9b7eae340d7eb46ea4fe391fe0c37738
ftf <xwp_fullstack@163.com>
2019-04-11 10:17:18 +0800

    成果需求时间问题

--- achievement/serializers.py
+++ achievement/serializers.py
@@ -78,7 +78,7 @@ class KeywordsInfoSerializer(serializers.ModelSerializer):
 
 # 成果信息表序列化
 class ResultsInfoSerializer(serializers.ModelSerializer):
-    insert_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M", required=False, read_only=True)
+    insert_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
     expiry_dateb = serializers.DateTimeField(format="%Y-%m-%d", required=False,allow_null=True)
     expiry_datee = serializers.DateTimeField(format="%Y-%m-%d", required=False,allow_null=True)
     rexpiry_dateb = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False,allow_null=True)
