9ad70d16030d5c815c6a767e049daf4ce516c830
ftf <xwp_fullstack@163.com>
2019-10-09 11:32:05 +0800

    成果需求业务逻辑修改

--- index/views.py
+++ index/views.py
@@ -49,32 +49,14 @@ class Index(APIView):
             'consult_reply_count':consult_reply_count
         })
 
-def fun1(list_data,list_result,list_requirement,num):
+map = {
+    1: [ResultsInfo, RequirementsInfo],
+    2: [BrokerBaseinfo, CollectorBaseinfo],
+    3: [AccountInfo],
+}
 
-    for i in range(0, num):
-        da = datetime.date.today()
-        d3 = da - datetime.timedelta(days=i)
-        result_count = ResultsInfo.objects.filter(show_state=1, insert_time__contains=d3).count()
-        requirement_count = RequirementsInfo.objects.filter(show_state=1, insert_time__contains=d3).count()
-        list_data.append(d3)
-        list_result.append(result_count)
-        list_requirement.append(requirement_count)
-    return list_data, list_result, list_requirement
 
-def fun2(list_data,list_result,list_requirement,num):
-
-    for i in range(0, num, 30):
-        da = datetime.date.today()
-        d3 = da - datetime.timedelta(days=i)
-        d4 = da - datetime.timedelta(days=i + 30)
-        result_count = ResultsInfo.objects.filter(show_state=1, insert_time__gt=d4, insert_time__lt=d3).count()
-        requirement_count = RequirementsInfo.objects.filter(show_state=1, insert_time__gt=d4, insert_time__lt=d3).count()
-        list_data.append(d3)
-        list_result.append(result_count)
-        list_requirement.append(requirement_count)
-    return list_data, list_result, list_requirement
-
-def fun3(list_data,list_result,list_requirement,date2,date3):
+def fun3(list_data,list_one,list_two,date2,date3,ma):
     for i in range(0, date3):
         date44 = date2 - datetime.timedelta(days=i)
         date4 = str(date44) + ' ' + '00:00:00'
@@ -82,30 +64,57 @@ def fun3(list_data,list_result,list_requirement,date2,date3):
         date4 = datetime.datetime.strptime(date4, '%Y-%m-%d %H:%M:%S')
         date5 = datetime.datetime.strptime(date5, '%Y-%m-%d %H:%M:%S')
 
-        result_count = ResultsInfo.objects.filter(show_state=1, insert_time__gt=date5, insert_time__lt=date4).count()
-        requirement_count = RequirementsInfo.objects.filter(show_state=1, insert_time__gt=date5, insert_time__lt=date4).count()
-        list_data.append(date44)
-        list_result.append(result_count)
-        list_requirement.append(requirement_count)
-    return list_data, list_result, list_requirement
+        if ma == 1:
+            result_count = map[ma][0].objects.filter(show_state=1, insert_time__gt=date5, insert_time__lt=date4).count()
+            requirement_count = map[ma][1].objects.filter(show_state=1, insert_time__gt=date5, insert_time__lt=date4).count()
+            list_data.append(date44)
+            list_one.append(result_count)
+            list_two.append(requirement_count)
+        elif ma == 2:
+            result_count = map[ma][0].objects.filter(state=1, insert_time__gt=date5, insert_time__lt=date4).count()
+            requirement_count = map[ma][1].objects.filter(state=1, insert_time__gt=date5,
+                                                          insert_time__lt=date4).count()
+            list_data.append(date44)
+            list_one.append(result_count)
+            list_two.append(requirement_count)
+        else:
+            result_count = map[ma][0].objects.filter(state=1, account=None, insert_time__gt=date5, insert_time__lt=date4).count()
+            list_data.append(date44)
+            list_one.append(result_count)
 
-def fun4(list_data,list_result,list_requirement,date2,date3):
+    return list_data, list_one, list_two
+
+def fun4(list_data,list_one,list_two,date2,date3,ma):
     for i in range(0, date3, 30):
         date4 = date2 - datetime.timedelta(days=i)
         date5 = date2 - datetime.timedelta(days=i+30)
-        result_count = ResultsInfo.objects.filter(show_state=1, insert_time__gt=date5, insert_time__lt=date4).count()
-        requirement_count = RequirementsInfo.objects.filter(show_state=1, insert_time__gt=date5, insert_time__lt=date4).count()
-        list_data.append(date4)
-        list_result.append(result_count)
-        list_requirement.append(requirement_count)
-    return list_data, list_result, list_requirement
+        if ma == 1:
+            result_count = map[ma][0].objects.filter(show_state=1, insert_time__gt=date5, insert_time__lt=date4).count()
+            requirement_count = map[ma][1].objects.filter(show_state=1, insert_time__gt=date5,
+                                                          insert_time__lt=date4).count()
+            list_data.append(date4)
+            list_one.append(result_count)
+            list_two.append(requirement_count)
+        elif ma == 2:
+            result_count = map[ma][0].objects.filter(state=1, insert_time__gt=date5, insert_time__lt=date4).count()
+            requirement_count = map[ma][1].objects.filter(state=1, insert_time__gt=date5,
+                                                          insert_time__lt=date4).count()
+            list_data.append(date4)
+            list_one.append(result_count)
+            list_two.append(requirement_count)
+        else:
+            result_count = map[ma][0].objects.filter(state=1, account=None, insert_time__gt=date5, insert_time__lt=date4).count()
+            list_data.append(date4)
+            list_one.append(result_count)
+
+    return list_data, list_one, list_two
 
 class ResultIndex(APIView):
     permission_classes = (permissions.IsAuthenticated, ReadOnlyPermission)
     def get(self, request):
 
         # 创建空数组
-        list_data = []
+        list_data1 = []
         list_result = []
         list_requirement = []
 
@@ -120,11 +129,84 @@ class ResultIndex(APIView):
         date2 = datetime.datetime.strptime(date2, '%Y-%m-%d').date()
         date3 = (date2 - date1).days
         if date3 <= 30:
-            list_data, list_result, list_requirement = fun3(list_data, list_result, list_requirement, date2, date3)
+            a, b, c = fun3(list_data1, list_result, list_requirement, date2, date3, 1)
         else:
-            list_data, list_result, list_requirement = fun4(list_data, list_result, list_requirement, date2, date3)
+            a, b, c = fun4(list_data1, list_result, list_requirement, date2, date3, 1)
 
-        # if not params:
+        # 返回相应的数据格式
+        return JsonResponse({
+            'list_data1': a,
+            'list_result': b,
+            'list_requirement': c
+
+        })
+
+class BrokerIndex(APIView):
+    permission_classes = (permissions.IsAuthenticated, ReadOnlyPermission)
+    def get(self, request):
+        # 创建空数组
+        list_data2 = []
+        list_broker = []
+        list_collector = []
+
+        # 判断参数在某个时间段显示的信息
+        params = request.query_params
+
+        date_params = params.get('date_params', None)
+        date_params = date_params.split(',')
+        date1 = date_params[0]
+        date2 = date_params[1]
+        date1 = datetime.datetime.strptime(date1, '%Y-%m-%d').date()
+        date2 = datetime.datetime.strptime(date2, '%Y-%m-%d').date()
+        date3 = (date2 - date1).days
+        if date3 <= 30:
+            a, b, c = fun3(list_data2, list_broker, list_collector, date2, date3, 2)
+        else:
+            a, b, c = fun4(list_data2, list_broker, list_collector, date2, date3, 2)
+
+        # 返回相应的数据格式
+        return JsonResponse({
+            'list_data2': a,
+            'list_broker': b,
+            'list_collector': c
+
+        })
+
+class AccountIndex(APIView):
+    permission_classes = (permissions.IsAuthenticated, ReadOnlyPermission)
+    def get(self, request):
+        # 创建空数组
+        list_data3 = []
+        list_AccountInfo = []
+        list_none = []
+
+        # 判断参数在某个时间段显示的信息
+        params = request.query_params
+
+        date_params = params.get('date_params', None)
+        date_params = date_params.split(',')
+        date1 = date_params[0]
+        date2 = date_params[1]
+        date1 = datetime.datetime.strptime(date1, '%Y-%m-%d').date()
+        date2 = datetime.datetime.strptime(date2, '%Y-%m-%d').date()
+        date3 = (date2 - date1).days
+        if date3 <= 30:
+            a, b, c = fun3(list_data3, list_AccountInfo, list_none, date2, date3, 3)
+        else:
+            a, b, c = fun4(list_data3, list_AccountInfo, list_none, date2, date3, 3)
+
+        # 返回相应的数据格式
+        return JsonResponse({
+            'list_data3': a,
+            'list_AccountInfo': b,
+            'list_none': c
+
+        })
+
+
+
+
+# if not params:
         #     # 默认显示一周的信息
         #     list_data, list_result, list_requirement = fun1(list_data, list_result, list_requirement, 7)
         #
@@ -160,22 +242,28 @@ class ResultIndex(APIView):
         #     else:
         #         list_data, list_result, list_requirement = fun4(list_data, list_result, list_requirement, date2, date3)
         # 返回相应的数据格式
-        return JsonResponse({
-            'list_data': list_data,
-            'list_result': list_result,
-            'list_requirement': list_requirement,
-
-        })
-
-class BrokerIndex(APIView):
-    permission_classes = (permissions.IsAuthenticated, ReadOnlyPermission)
-    def get(self, request):
-        return JsonResponse({
-            'ceshi':'ceshi'
-
-        })
-
-class AccountIndex(APIView):
-    permission_classes = (permissions.IsAuthenticated, ReadOnlyPermission)
-    def get(self, request):
-        pass
+    
+# def fun1(list_data,list_result,list_requirement,num):
+# 
+#     for i in range(0, num):
+#         da = datetime.date.today()
+#         d3 = da - datetime.timedelta(days=i)
+#         result_count = ResultsInfo.objects.filter(show_state=1, insert_time__contains=d3).count()
+#         requirement_count = RequirementsInfo.objects.filter(show_state=1, insert_time__contains=d3).count()
+#         list_data.append(d3)
+#         list_result.append(result_count)
+#         list_requirement.append(requirement_count)
+#     return list_data, list_result, list_requirement
+# 
+# def fun2(list_data,list_result,list_requirement,num):
+# 
+#     for i in range(0, num, 30):
+#         da = datetime.date.today()
+#         d3 = da - datetime.timedelta(days=i)
+#         d4 = da - datetime.timedelta(days=i + 30)
+#         result_count = ResultsInfo.objects.filter(show_state=1, insert_time__gt=d4, insert_time__lt=d3).count()
+#         requirement_count = RequirementsInfo.objects.filter(show_state=1, insert_time__gt=d4, insert_time__lt=d3).count()
+#         list_data.append(d3)
+#         list_result.append(result_count)
+#         list_requirement.append(requirement_count)
+#     return list_data, list_result, list_requirement
